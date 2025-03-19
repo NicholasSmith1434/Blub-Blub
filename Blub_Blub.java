efe13a15b56fa1e76539d2e687c15f565209e8e5
Nicholas Smith <157626157+NicholasSmith1434@users.noreply.github.com>
2025-03-19 12:01:44 -0700

    Add files via upload

--- Blub_Blub.java
+++ Blub_Blub.java
@@ -1,8 +1,14 @@
-package com.blub.lwjgl3;
+package com.blub;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.blub.lwjgl3.Screens.MainMenuScreen;
+import com.blub.Screens.MainMenuScreen;
+import com.blub.Screens.PetScreen;
+import com.blub.Screens.PauseMenuScreen;
+import com.blub.Screens.SaveScreen;
 
 public class Blub_Blub extends Game {
 
@@ -12,17 +18,86 @@ public class Blub_Blub extends Game {
 
     public SpriteBatch batch;
 
+    public static Music backgroundMusic;
+    public static Sound click;
+    public static Sound bonk;
+
+    //private PauseSettings pauseSettings;
+    private PauseMenuScreen pauseMenuScreen;
+    private PetScreen petScreen;
+    private MainMenuScreen mainMenuScreen;
+    private SaveScreen saveScreen;
+
+    public final static int MENU = 0;
+    public final static int PAUSE = 1;
+    public final static int PET = 2;
+    public final static int SOUND = 3;
+    public final static int SAVE = 4;
+
     //Loads Main Menu off the start
     @Override
     public void create() {
+        //Load music file (once)
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("LoungeLover.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.45f);
+        backgroundMusic.play();
+
+        //Load SFXs
+        click = Gdx.audio.newSound(Gdx.files.internal("click.mp3"));
+        bonk = Gdx.audio.newSound(Gdx.files.internal("bonk.mp3"));
+
+        //pauseSettings = new PauseSettings();
+
         batch = new SpriteBatch();
-        this.setScreen(new MainMenuScreen(this));
+
+        setScreen(new MainMenuScreen(this));
+    }
+
+    //public PauseSettings getPauseSettings(){
+    //    return this.pauseSettings;
+    //}
+
+    public void changeScreen(int screen){
+        switch(screen){
+            case MENU:
+                if(mainMenuScreen == null) mainMenuScreen = new MainMenuScreen(this);
+                this.setScreen(mainMenuScreen);
+                break;
+            case PAUSE:
+                if(pauseMenuScreen == null) pauseMenuScreen = new PauseMenuScreen(this);
+                this.setScreen(pauseMenuScreen);
+                break;
+            case PET:
+                if(petScreen == null) petScreen = new PetScreen(this);
+                this.setScreen(petScreen);
+                break;
+            case SAVE:
+                if(saveScreen == null) saveScreen = new SaveScreen(this);
+                this.setScreen(saveScreen);
+                break;
+        }
+    }
+
+    public void playClick(){
+        click.play();
+    }
+
+    public void playBonk(){
+        bonk.play();
     }
 
     @Override
     public void render() {
         super.render();
         //System.out.print("" + Gdx.graphics.getDeltaTime()); --> Used to count delta time
+    }
+
+    @Override
+    public void dispose() {
+        backgroundMusic.dispose();
+        click.dispose();
+        batch.dispose();
     }
 }
 
--- PetScreen.java
+++ PetScreen.java
@@ -1,26 +1,31 @@
-package com.blub.lwjgl3.Screens;
+package com.blub.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.blub.lwjgl3.Blub_Blub;
+import com.blub.Blub_Blub;
 
 public class PetScreen implements Screen {
 
     Blub_Blub game;
     float x, y;
 
-    Texture img;
+    private static final float PAUSE_BUTTON_WIDTH = 80;
+    private static final float PAUSE_BUTTON_HEIGHT = 80;
 
+    private static final float SPHEAL_WIDTH = 560;
+    private static final float SPHEAL_HEIGHT = 560;
+
+    Texture portrait;
     //Pet Standing idle, clicked on head, clicked on belly, clicked on face
     Texture petIdle;
     Texture petPatted;
     Texture petPoked;
     Texture petBooped;
     //Mini-game buttons
-    Texture playButtonActive;
-    Texture playButtonInactive;
+    Texture gameButtonActive;
+    Texture gameButtonInactive;
     Texture cleanButtonActive;
     Texture cleanButtonInactive;
     Texture feedButtonActive;
@@ -29,36 +34,97 @@ public class PetScreen implements Screen {
     Texture pauseButtonActive;
     Texture pauseButtonInactive;
 
+    //Spheal
+    Texture sphealTest;
+    Texture sphealPoke;
+
 
     public PetScreen(Blub_Blub game) {
         this.game = game;
 
-        petIdle = new Texture(Gdx.files.internal("blank"));
-        petPatted = new Texture(Gdx.files.internal("blank"));
-        petPoked = new Texture(Gdx.files.internal("blank"));
-        petBooped = new Texture(Gdx.files.internal("blank"));
-        playButtonActive = new Texture(Gdx.files.internal("blank"));
-        playButtonInactive = new Texture(Gdx.files.internal("blank"));
-        cleanButtonActive = new Texture(Gdx.files.internal("blank"));
-        cleanButtonInactive = new Texture(Gdx.files.internal("blank"));
-        feedButtonActive = new Texture(Gdx.files.internal("blank"));
-        feedButtonInactive = new Texture(Gdx.files.internal("blank"));
-        pauseButtonActive = new Texture(Gdx.files.internal("blank"));
-        pauseButtonInactive = new Texture(Gdx.files.internal("blank"));
+        portrait = new Texture("PetPortrait.png");
+        petIdle = new Texture(Gdx.files.internal("libgdx16.png"));
+        petPatted = new Texture(Gdx.files.internal("libgdx16.png"));
+        petPoked = new Texture(Gdx.files.internal("libgdx16.png"));
+        petBooped = new Texture(Gdx.files.internal("libgdx16.png"));
+        gameButtonActive = new Texture(Gdx.files.internal("libgdx16.png"));
+        gameButtonInactive = new Texture(Gdx.files.internal("libgdx16.png"));
+        cleanButtonActive = new Texture(Gdx.files.internal("libgdx16.png"));
+        cleanButtonInactive = new Texture(Gdx.files.internal("libgdx16.png"));
+        feedButtonActive = new Texture(Gdx.files.internal("libgdx16.png"));
+        feedButtonInactive = new Texture(Gdx.files.internal("libgdx16.png"));
+        pauseButtonActive = new Texture(Gdx.files.internal("PauseButtonActive.png"));
+        pauseButtonInactive = new Texture(Gdx.files.internal("PauseButtonInactive.png"));
+        sphealTest = new Texture(Gdx.files.internal("sphealTest.png"));
+        sphealPoke = new Texture(Gdx.files.internal("sphealPoke.png"));
     }
 
     @Override
     public void show() {
-        img = new Texture("badlogic.jpg");
     }
 
     @Override
     public void render(float v) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0.99f, 0.9f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-        game.batch.draw(img, x, y);
+
+        //Portrait
+        game.batch.draw(portrait, Blub_Blub.WIDTH / 2 - portrait.getWidth() / 4,
+            Blub_Blub.HEIGHT / 2 - portrait.getHeight() / 6, portrait.getWidth() / 2, portrait.getHeight() / 2);
+
+        //Sprite
+
+        if((Gdx.input.getX() > Blub_Blub.WIDTH / 2 - SPHEAL_WIDTH / 2 && Gdx.input.getX() < Blub_Blub.WIDTH / 2 +
+            SPHEAL_WIDTH / 2 && Blub_Blub.HEIGHT - Gdx.input.getY() > Blub_Blub.HEIGHT / 2 - SPHEAL_HEIGHT / 3
+        && Blub_Blub.HEIGHT- Gdx.input.getY() < Blub_Blub.HEIGHT / 2 - SPHEAL_HEIGHT / 3 + SPHEAL_HEIGHT)){
+            game.batch.draw(sphealTest, Blub_Blub.WIDTH / 2 - SPHEAL_WIDTH / 2,
+                Blub_Blub.HEIGHT / 2 - SPHEAL_HEIGHT / 3, SPHEAL_WIDTH, SPHEAL_HEIGHT);
+            if(Gdx.input.isTouched()){
+                game.batch.draw(sphealPoke, Blub_Blub.WIDTH / 2 - SPHEAL_WIDTH / 2,
+                    Blub_Blub.HEIGHT / 2 - SPHEAL_HEIGHT / 3, SPHEAL_WIDTH, SPHEAL_HEIGHT);
+                game.playBonk();
+
+            }
+        } else {
+            game.batch.draw(sphealTest, Blub_Blub.WIDTH / 2 - SPHEAL_WIDTH / 2,
+                Blub_Blub.HEIGHT / 2 - SPHEAL_HEIGHT / 3, SPHEAL_WIDTH, SPHEAL_HEIGHT);
+        }
+
+
+        //Pause Button
+        float pauseButtonX = Blub_Blub.WIDTH - 100;
+        float pauseButtonY = Blub_Blub.HEIGHT - 100;
+
+        if (Gdx.input.getX() > pauseButtonX && Gdx.input.getX() < pauseButtonX + PAUSE_BUTTON_WIDTH &&
+            Blub_Blub.HEIGHT - Gdx.input.getY() > pauseButtonY && Blub_Blub.HEIGHT - Gdx.input.getY()
+            < pauseButtonY + PAUSE_BUTTON_HEIGHT) {
+
+            game.batch.draw(pauseButtonActive, pauseButtonX, pauseButtonY, PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT);
+
+            //Pause button clicked
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new PauseMenuScreen(game)); //Closes (exits) game
+            }
+        } else {
+            game.batch.draw(pauseButtonInactive, pauseButtonX, pauseButtonY, PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT);
+        }if (Gdx.input.getX() > pauseButtonX && Gdx.input.getX() < pauseButtonX + PAUSE_BUTTON_WIDTH &&
+            Blub_Blub.HEIGHT - Gdx.input.getY() > pauseButtonY && Blub_Blub.HEIGHT - Gdx.input.getY()
+            < pauseButtonY + PAUSE_BUTTON_HEIGHT) {
+
+            game.batch.draw(pauseButtonActive, pauseButtonX, pauseButtonY, PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT);
+
+            //Pause button clicked
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.playClick();
+                game.setScreen(new PauseMenuScreen(game));
+            }
+        } else {
+            game.batch.draw(pauseButtonInactive, pauseButtonX, pauseButtonY, PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT);
+        }
         game.batch.end();
     }
 
